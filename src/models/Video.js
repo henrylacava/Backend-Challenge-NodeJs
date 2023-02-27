ddbe5e4ddd69dc70f7e69ab22ca635bff77136b1
Henry <109253086+henrylacava@users.noreply.github.com>
2023-02-27 15:13:35 -0300

    Update Video.js

--- src/models/Video.js
+++ src/models/Video.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const videoSchema = new mongoose.Schema(
     {
         id: {type: String},
-        Título: {type: String, required: true},
-        Descrição: {type: String, required: true},
-        URL: {type: String, required: true}
+        Título: {type: String, required: true, maxlength: 30},
+        Descrição: {type: String, required: true, maxlength: 30},
+        URL: {type: String, required: true, match: [/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/, 'Please fill a valid URL'] }
     }
 )
 
